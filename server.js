359492da6a3f26823731e5f757028f9e8fb1a699
Olive251 <oliviataylor437@gmail.com>
2022-02-03 16:48:16 -0500

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const xps = require("express");
 const res = require("express/lib/response");
 const path = require("path");
+const bSvc = require("./blog-service.js");
 
 const app = xps();
 
@@ -15,6 +16,23 @@ app.get('/', (req,res) => {
 app.get('/about', (req,res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 })
+
+//update to send JSON formatted string containing all posts in posts.json with published==true
+app.get('./blog', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
+})
+
+//json string all posts in posts.json
+app.get('./posts', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'posts.html'));
+})
+
+//json string categories in categories.json
+app.get('./categories', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'categories.html'));
+})
+
+//add 404 error handler
 
 app.listen(port, () => {
     console.log(`App is listening at http://localhost:${port}`);
